d021d266ff11370bad6d184794ea38bf513a9b20
Osama Zarraa <osamaz26@gmail.com>
2020-04-25 02:06:01 +0200

    fix(users controller): return service result

--- src/main/java/se/lnu/ems/backend/controllers/api/users/UsersController.java
+++ src/main/java/se/lnu/ems/backend/controllers/api/users/UsersController.java
@@ -32,6 +32,6 @@ public class UsersController {
         if (result.hasErrors()) {
             return result.getAllErrors();
         }
-        return usersService;
+        return usersService.create();
     }
 }

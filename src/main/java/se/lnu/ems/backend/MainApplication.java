ae78a4a4b39f12c19920c4be0105eaacd1b0f300
jacobYousif <jy222bz@student.lnu.se>
2020-05-03 00:05:57 +0200

    docs(Main):

--- src/main/java/se/lnu/ems/backend/MainApplication.java
+++ src/main/java/se/lnu/ems/backend/MainApplication.java
@@ -4,16 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * The main class for the main entry point for MyExam application.
- *
- * @author Group 1
+ * Linnaeus University.
+ * 2DV609 - Project Course in Software Engineering.
+ * <p>
+ * @author Group 1: Abdalrahman Dabor, Ahmad Anbarje, Hamza Hajderovic,
+ * Jacob Yousif, Osama Zarraa and Sirwan Rasoul.
  * @version 1.0
  * @since 2020-05-01
+ * <p>
+ * The main class for the main entry point for the MyExam application.
  */
 @SpringBootApplication
 public class MainApplication {
     /**
      * The main entry point to the application.
+     *
      * @param args the arguments.
      */
     public static void main(String[] args) {
